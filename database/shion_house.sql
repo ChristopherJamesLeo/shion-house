b399db71ad69aa4f501fab3bcf1b644cab338088
Christopher James Leo <aungaungoo.hotmail@gmail.com>
2023-03-19 20:53:17 +0630

    final

--- database/shion_house.sql
+++ database/shion_house.sql
@@ -5,20 +5,20 @@ CREATE TABLE IF NOT EXISTS products (
     name CHAR(100) NOT NULL DEFAULT "Product Name",
     type CHAR(100) NOT NULL DEFAULT "type",
     price DOUBLE(5,2) ,
-    img BLOB ,
+    img BLOB NOT NULL ,
     create_at TIMESTAMP DEFAULT NOW() ,
     update_at TIMESTAMP DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP,
 )
 
 INSERT INTO products(name,type,price,img) VALUES
-("Hoddie","cloths",32,"arrival1.png.wbep"),
-("Maphala","assecories",12,"arrival2.png.wbep"),
-("Shirt","Clothes",23,"arrival3.png.wbep"),
-("Blue Shirt","Clothes",34,"arrival4.png.wbep"),
-("Garwon","Girl Cloth",10,"arrival5.png.wbep"),
-("Glasses","assecories",50,"arrival6.png.wbep"),
-("White Pin","Clothes",20,"arrival7.png.wbep"),
-("Addidas","Shoe",60,"arrival8.png.wbep");
+("Hoddie","cloths",32,"arrival1.png.webp"),
+("Maphala","assecories",12,"arrival2.png.webp"),
+("Shirt","Clothes",23,"arrival3.png.webp"),
+("Blue Shirt","Clothes",34,"arrival4.png.webp"),
+("Garwon","Girl Cloth",10,"arrival5.png.webp"),
+("Glasses","assecories",50,"arrival6.png.webp"),
+("White Pin","Clothes",20,"arrival7.png.webp"),
+("Addidas","Shoe",60,"arrival8.png.webp");
 
 CREATE TABLE IF NOT EXISTS users(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -26,14 +26,14 @@ CREATE TABLE IF NOT EXISTS users(
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     role CHAR(100) NOT NULL,
+    img BLOB NOT NULL,
     address VARCHAR(255) NULL,
     contactno VARCHAR(255) NOT NULL DEFAULT "123456789",
     create_at TIMESTAMP DEFAULT NOW()
 )
-INSERT INTO users (name,email,password,role,address,contactno) VALUES
-("james leo","james@gmail.com","11111","1","Oaktharmyothit","0912312422"),
-("aung aung","aung@gmail.com","22222","0","Maharmying","0998765432"),
-("hla hla","hlahla@gmail.com","33333","1","pinsi","09872868342");
+INSERT INTO users (name,email,password,role,img,address,contactno) VALUES
+("james","james@gmail.com","11111","1","user2.jpg","sarilanka","0912312422"),
+("vivian","vivian@gmail.com","22222","0","agent-2.jpg","singapore","09872868342");
 
 
 CREATE TABLE IF NOT EXISTS messages(
